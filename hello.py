f05eec1b24d5d4dd1bdecd43b090e08ea9fb1c71
Sanket <Sanket151@aol.com>
2019-01-27 20:42:12 +0530

    check 2

---
+++ hello.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Index Page"
+
+@app.route("/hello")
+def hello():
+    return "Hello World!"
+
+
+@app.route("/members")
+def members():
+    return "Members"
+
+
+@app.route("/members/<string:name>/")
+def getMember(name):
+    return name < / string: name >
+
+if __name__ == "__main__":
+    app.run()
