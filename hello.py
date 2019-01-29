5088cecb6cc1c5ee70abd99aa375030fa4ed6ab9
Sanket <Sanket151@aol.com>
2019-01-29 18:20:13 +0530

    check 3

--- hello.py
+++ hello.py
@@ -1,24 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def index():
     return "Index Page"
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
 
 @app.route("/hello")
 def hello():
     return "Hello World!"
 
-
 @app.route("/members")
 def members():
     return "Members"
 
+@app.route('/abc',  methods=['POST'])
+def success():
+   if request.method == 'POST':
+       email = request.form['email']
+       return render_template('success.html', email=email)
+   else:
+       pass
 
-@app.route("/members/<string:name>/")
-def getMember(name):
-    return name < / string: name >
+#@app.route("/members/<string:name>/")
+#def getMember(name):
+#    return </string:name>
 
 if __name__ == "__main__":
     app.run()
